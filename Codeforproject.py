7b7cb04483649d5a904a00e63f2f90981bb79e2b
Flavia Malena Paz <39998871+FlaviaPaz@users.noreply.github.com>
2022-03-03 10:14:51 +0100

    oadjsi
    inicio

---
+++ Codeforproject.py
@@ -0,0 +1,13 @@
+import getopt
+from gettext import install
+from msilib.schema import Upgrade
+from platform import python_implementation
+from select import select
+from types import BuiltinFunctionType
+from webbrowser import get
+import pip
+import csv
+# with open("peptides.csv", 'r') as f:
+#    wines = list(csv.reader(f, delimiter=";"))
+#import numpy as np
+#wines = np.array(wines[1:], dtype=np.float)
